1fc0559f4830442df314cbc701b284fa317d20fc
MEDescuento <medescuentosas@gmail.com>
2025-09-08 23:35:44 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/medescuento-renewed-web/', // 👈 IMPORTANTE para GitHub Pages
+  base: '/Pagina-web-medescuento/', // 👈 IMPORTANTE para GitHub Pages
   server: {
     host: "::",
     port: 8080,
