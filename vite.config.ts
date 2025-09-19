272e07ae9d6337947b626438929505465a3e76e8
andreajg1024 <andreejaraguti@gmail.com>
2025-09-19 11:49:49 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/medescuento-renewed-web/', // 👈 IMPORTANTE para GitHub Pages
+  base: '/',
   server: {
     host: "::",
     port: 8080,
