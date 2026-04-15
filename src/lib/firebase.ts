3b6647da63f7e159487354b0f29783bb00515bcb
andreajg1024 <andreejaraguti@gmail.com>
2026-04-15 14:08:27 -0500

    firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,16 +8,21 @@ import {
   setDoc
 } from "firebase/firestore";
 
+// Tu configuración de Firebase (la que te dio Firebase Console)
 const firebaseConfig = {
-  apiKey: "AQUÍ_TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyDJY5NLOJKI9C8C319nH1mRRqyz740K7mU",
+  authDomain: "medescuento-1a2ff.firebaseapp.com",
+  projectId: "medescuento-1a2ff",
+  storageBucket: "medescuento-1a2ff.firebasestorage.app",
+  messagingSenderId: "246539507592",
+  appId: "1:246539507592:web:bbf08f6aa8c8574b684c3d"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exportar Firestore
 export const db = getFirestore(app);
+
+// Exportar funciones necesarias
 export { doc, getDoc, updateDoc, increment, setDoc };
