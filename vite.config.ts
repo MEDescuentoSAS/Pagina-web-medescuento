2946706c25845f85efac523f365d66c950af11bd
andreajg1024 <andreejaraguti@gmail.com>
2026-06-02 21:02:07 -0500

    cambio en vite con el dominio

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    allowedHosts: ['www.medescuento.com'],
+  },
   plugins: [
     react(),
     mode === 'development' &&
